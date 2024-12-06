149fb523ccd0bd0b5239d35c84495a5c120f4659
shubhamsingh123-sk <shubhamsingh94070@gmail.com>
2024-12-06 12:08:18 +0530

    Add files via upload

---
+++ portfolio_ website/script.js
@@ -0,0 +1,7 @@
+// JavaScript for mobile menu toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
